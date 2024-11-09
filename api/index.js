a0bca65a33afdcc5a8ea2361ce6f2f31a7829b42
Yashoda Farswan <farswangolu@gmail.com>
2024-11-09 13:36:47 +0530

    Project completed

--- api/index.js
+++ api/index.js
@@ -10,124 +10,132 @@ const cookieParser = require('cookie-parser');
 const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
-const { createDeflate } = require('zlib');
 
-const salt = bcrypt.genSaltSync(10);    // to create hash of the registered password
+const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfasdfqe2343asdfdasd';
-
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-
 mongoose.connect('mongodb+srv://farswangolu:XvV2JeI9157pk41E@cluster0.ofjnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-const connection = mongoose.connection;
-connection.on("error", (e) => {
-    console.log(e);
-})
 
-connection.once("open" , () => console.log("connencted to mongoose"))
-// console.log(connection)
-
-
-// app.get('/api', async(req,res) => {
-//     res.send({
-//         name: "Golu",
-//         age: "Byah Ki"
-//     })
-// })
- 
-
-// Route to create new user
 app.post('/register', async (req,res) => {
-    const {username,password} = req.body;
-    try{
-        const userDoc = await User.create({
-            username,
-            password:bcrypt.hashSync(password,salt)
-            });
-        res.json(userDoc);
-
-    } catch(e){
-        console.log(e);
-        res.status(400).json(e);
-    }
-});
-
-app.post('/login', async (req,res) =>{
-    const {username,password} = req.body;
-    const userDoc = await User.findOne({username});
-
-    const passOk = bcrypt.compareSync(password, userDoc.password);
-    if(passOk){
-        //logged in 
-        jwt.sign({username, id:userDoc._id}, secret, {} , (err,token) => {
-            if(err) throw err;
-            res.cookie('token', token).json({
-                id:userDoc._id,
-                username,
-            });
-        });
-        
-    }
-    else{
-        res.status(400).json('wrong credentials');
-    }
-});
-
-app.get('/profile',(req,res) => {
-    const {token} = req.cookies;
-    jwt.verify(token, secret, {}, (err, info) => {
-        if(err) throw err;
-        res.json(info);
+  const {username,password} = req.body;
+  try{
+    const userDoc = await User.create({
+      username,
+      password:bcrypt.hashSync(password,salt),
     });
-    res.json(req.cookies);
+    res.json(userDoc);
+  } catch(e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
 });
 
-app.post('/logout',(req,res) => {
-    res.cookie('token', '').json('ok');
-})
+app.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const userDoc = await User.findOne({username});
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    // logged in
+    jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id:userDoc._id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json('wrong credentials');
+  }
+});
 
-app.post('/post', uploadMiddleware.single('file'),async (req,res) =>{
-    const {originalname,path} = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.length -1];
-    const newPath = path +'.'+ ext;
-    fs.renameSync(path, newPath);
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
 
-    const {token} = req.cookies;
-    jwt.verify(token, secret, {}, async (err, info) => {
-        if(err) throw err;
-        const {title,summary,content} = req.body;
-        const postDoc = await Post.create({
-        title,
-        summary,
-        content,
-        cover:newPath,
-        author: info.id,
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
+});
+
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {title,summary,content} = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      cover:newPath,
+      author:info.id,
     });
     res.json(postDoc);
-    });  
+  });
+
+});
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.update({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
+
 });
 
 app.get('/post', async (req,res) => {
-    res.json(
-        await Post.find()
-        .populate('author', ['username'])
-        .sort({createdAt: -1})
-        .limit(20)
-    );
+  res.json(
+    await Post.find()
+      .populate('author', ['username'])
+      .sort({createdAt: -1})
+      .limit(20)
+  );
 });
 
-app.get('/post/:id', async (req,res) => {
-    const {id} = req.params;
-    const postDoc = await Post.findById(id);
-    res.json(postDoc);
+app.get('/post/:id', async (req, res) => {
+  const {id} = req.params;
+  const postDoc = await Post.findById(id).populate('author', ['username']);
+  res.json(postDoc);
 })
 
 app.listen(4000);
+//
 // XvV2JeI9157pk41E
 
 //mongodb+srv://farswangolu:<db_password>@cluster0.ofjnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-import {Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './Header';
-import Post from './Post';
+import Post from "./Post";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
 import Layout from "./Layout";
-import Indexpage from './pages/IndexPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import { UserContextProvider } from './UserContext';
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import {UserContextProvider} from "./UserContext";
 import CreatePost from "./pages/CreatePost";
-import PostPage from './pages/PostPage';
+import PostPage from "./pages/PostPage";
+import EditPost from "./pages/EditPost";
 
 function App() {
   return (
     <UserContextProvider>
-    <Routes>
-          <Route path = "/" element={<Layout/>}>
-            <Route index element={<Indexpage/>}/>
-            <Route path='/login' element = {<LoginPage/>}/>
-            <Route path = '/register' element={<RegisterPage />}/>
-            <Route path="/create" element={<CreatePost />} />
-            <Route path="/post/:id" element = {<PostPage />} />
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+        </Route>
+      </Routes>
     </UserContextProvider>
-    
   );
 }
-
 
 export default App;
